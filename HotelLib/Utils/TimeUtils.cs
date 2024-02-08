eab419db4a5fe7f7553deb4fc1340e31560477d0
Flux <flustix@foxes4life.net>
2024-02-08 21:42:21 +0100

    Add time format function

---
+++ HotelLib/Utils/TimeUtils.cs
@@ -0,0 +1,33 @@
+﻿namespace HotelLib.Utils;
+
+public static class TimeUtils
+{
+    public static string Format(double time)
+    {
+        time /= 1000f;
+
+        if (time < 0)
+            time = Math.Abs(time);
+
+        var months = (int)time / 2592000;
+        var days = (int)time / 86400 % 30;
+        var hours = (int)time / 3600 % 24;
+        var minutes = (int)time / 60 % 60;
+        var seconds = (int)time % 60;
+
+        var timeString = "";
+
+        if (months > 0)
+            timeString += $"{months}mon ";
+        else if (days > 0)
+            timeString += $"{days}d ";
+        else if (hours > 0)
+            timeString += $"{hours}h ";
+        else if (minutes > 0)
+            timeString += $"{minutes}m ";
+        else if (seconds > 0)
+            timeString += $"{seconds}s ";
+
+        return timeString.TrimEnd();
+    }
+}
